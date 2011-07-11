de3d9d30a7dce0f2c55ec2515bac4712b24ffc0d
andrey.schmitz@gmail.com <andrey.schmitz@gmail.com@4348ae4c-d2ae-f2fc-3d50-e0a6c441ee88>
2011-07-11 12:43:46 +0000

    #1

---
+++ restoran/go.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+require_once 'conf/globals.php';
+require_once 'lib/class.db.php';
+require_once 'lib/class.common.php';
+require_once 'lib/class.string.php';
+
+
+/* ------------ ÏÎÄÊËÞ×ÀÅÌÑß Ê ÁÀÇÅ ÄÀÍÍÛÕ -------------- */
+$db=new db(DB_NAME, DB_HOST, DB_USER, DB_PASS);
+
+if (isset($_GET['target'])) {
+	
+	$id=String::secure_user_input($_GET['target']);
+	
+	$banner=$db->get_single("SELECT * FROM fw_banners WHERE id='$id'");
+	
+	if (isset($banner['id'])) {
+		
+		$db->query("UPDATE fw_banners SET clicks=clicks+1 WHERE id='$id'");
+		
+		$location=$banner['target_url'];
+		header("Location: $location");
+		
+	}
+	else {
+		
+		$location=$_SERVER['HTTP_REFERER'];
+		header("Location: $location");
+	}
+	
+}
+
+?>
