2bdb448028db94b7aa121628e93ccab61c59e19b
Андрей Шмиц <andrey.schmitz@gmail.com>
2012-07-18 15:34:15 +0400

    shop-toy.ru

--- shop-toy.ru/age_parser.py
+++ shop-toy.ru/age_parser.py
@@ -29,31 +29,6 @@ def main():
     for item in items:
         grab.go(item['product_from'], log_dir=dir)
         if (grab.response.code == 200):
-            try:
-                imagelink = grab.xpath('//*[@class="catalog_list"]//tr//td[1]//a').get("href")
-                array = imagelink.replace('javascript:MM_openBrWindow2', '').replace('(','').replace(')','').replace('\'','').split(',')
-                image = array[0]
-                image_url = domain_parsing + image
-                imagearray = image.split('/')
-                if len(imagearray) > 0:
-                    imagename = imagearray[len(imagearray)-1]
-                
-                
-                imagedir = str(root) + '/uploaded_files/product_images/' + str(item['product_id'])
-                if (os.path.isdir(imagedir) == False):
-                    os.mkdir(imagedir,0777)
-
-                
-                
-                if (image_url != None and imagedir != None):
-                    fileimage = os.system("/usr/local/bin/wget -c --content-disposition -P %s %s" % (imagedir, image_url))
-
-                if (imagename != None):
-                    db.query("insert into product_images (product_id, image) values ('%d', '%s')" % (int(item['product_id']), str(imagename)))
-            
-            except IndexError:
-                print grab.response.url
-                print 'Ссылка на картинку не найдена'
                 
             try:
                 description = grab.xpath_text('//*[@class="catalog_list"]//tr//td[2]')
@@ -66,19 +41,21 @@ def main():
                         if age_txt != None:
                             age = re.search("\d", age_txt.group())
                             db.query("update fw_products set age = '%d' where id='%d'" % (int(age.group()), int(item['product_id'])))
+                            print "Обновлен возраст для продукта: %d" % int(item['product_id'])
 
-                        db.query("update fw_products set description = '%s' where id='%d'" % (db.escape(description.decode('utf-8').encode('cp1251')), int(item['product_id'])))
                     except UnicodeEncodeError:
-                        print "Ошибка перекодировки текста"
+                        print "Ошибка перекодировки"
                 
             except IndexError:
                 print grab.response.url
-                print "Описание категории не найденно"
+                print "Описание не найдено"
 
             
             
-            db.query("delete from _import_product_links where product_id='%d'" % int(item['product_id']))
-            
+            #db.query("delete from _import_product_links where product_id='%d'" % int(item['product_id']))
+    
+    print "Сделано."
+    
 DB_HOST, DB_NAME, DB_USER, DB_PASS = "localhost", "shop-toy", "demo", "gthtgenmt"
 db = DataBase.Db(DB_NAME,DB_HOST,DB_USER,DB_PASS)
 db.query("set CHARACTER SET cp1251")
